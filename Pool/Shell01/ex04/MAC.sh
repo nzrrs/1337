71aa374ff0f76d245c52bf3fa47a8f5bdf2fbc18
nzrrs <god.nizaryt10@gmail.com>
2026-07-15 13:02:55 +0100

    Finished shell01

--- Pool/Shell01/ex04/MAC.sh
+++ Pool/Shell01/ex04/MAC.sh
@@ -1,2 +1,2 @@
-#!bin/sh
+#!/bin/bash
 ip addr show | grep link/ether | awk '{print $2}'
--- Pool/Shell01/ex08/add_chelou.sh
+++ Pool/Shell01/ex08/add_chelou.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-FROM1="'\\\"?!"
 TO="01234"
-FROM2="mrdoc"
 
-#convert using translator sets 
+CONVERTED1=$(echo "$FT_NBR1" | tr "'\\\\\"?!" "$TO")
+CONVERTED2=$(echo "$FT_NBR2" | tr "mrdoc" "$TO")
 
-CONVERTED1=$(echo "$FT_NBR1" | tr "$FROM1" "$TO")
-CONVERTED2=$(echo "$FT_NBR2" | tr "$FROM2" "$TO")
-
-#changing the base of the converted from base 5 to base 10 
-
-CONVERTED1=$(echo "obase=10;ibase=5;"$CONVERTED1" | bc) 
-CONVERTED2=$(echo "obase=10;ibase=5;"$CONVERTED2" | bc) 
-
-#changing back to base 5 after calculating sum to replace with tr
-
-SUM=$(CONVERTED1 + CONVERTED2)
-
-echo "obase=13;ibase=13;"SUM"" | bc | tr "0123456789ABC" "gtaio luSnemf"
+echo "obase=13;ibase=5;$CONVERTED1+$CONVERTED2" | bc | tr "0123456789ABC" "gtaio luSnemf"
