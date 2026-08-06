38210035075d2a8505478bdf394ca2e38370eb81
Sn <164057221+nzrrs@users.noreply.github.com>
2026-08-06 15:44:26 +0100

    Update ft_advanced_sort_string_tab.c

--- Pool/C11/ex07/ft_advanced_sort_string_tab.c
+++ Pool/C11/ex07/ft_advanced_sort_string_tab.c
@@ -9,15 +9,6 @@
 /*   Updated: 2026/08/06 05:00:05 by nsadiki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] == s2[i] && s1[i])
-		i++;
-	return (s1[i] - s2[i]);
-}
 
 void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
 {
